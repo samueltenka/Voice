32da8ba0c26c0238d0d2ad146df263695857d59f
samueltenka <iguavadon@gmail.com>
2014-08-24 15:44:00 -0400

    Update Voice.py
    * optimized freq_search, making it faster (and less accurate, but still good to around 1Hz)
    * tested by writing "freq" which finds base-freq of each small segment of record; takes around 6processing-seconds/record-sec for segment-durations around 0.02 seconds; note that although this processing speed turns out to vary with segment-duration, it does so slowly because twice the number of segments => each segment half as long => half as long to process each segment, and they cancel.
    --- OUTCOME of TEST: got mixed 470/235 frequencies (true base was 235) but perhaps second harmonic strong. => TODO: don't use just strength as criterion in freq_search and freq; also use gcd relation to other freq.s, and history.

--- Voice.py
+++ Voice.py
@@ -2,18 +2,22 @@ import Record
 
 R = Record.Record()
 R.read_from('rec_ooh.wav')
-S = R.sub(0.0, 0.02)
-for f in range(230, 240):
-  print f, S.intensity_at(f)
 
 
-def freq_search(record, lbound, ubound, divs=5, layers=50):
+def freq_search(record, lbound=20, ubound=2000, divs=5, layers=5):
   diff = float(ubound-lbound)/divs
-  freqs = [lbound + i*diff for i in range(divs)]
-  c2, f = max((S.intensity_at(freq), freq) for freq in freqs)
+  freqs = [lbound + (i+0.5)*diff for i in range(divs)] ## 0.5 to center the freqs
+  c2, f = max((record.intensity_at(freq), freq) for freq in freqs)
   if layers==0:
     return abs(f)
   else:
-    ## so range is 4/5 of previous range (so go over same space
+    ## so range is 2/5 of previous range (so go over same space
     ## from many different "perspectives" (ways of slicing)):
-    return freq_search(record, f-2*diff, f+2*diff, divs, layers-1)
+    return freq_search(record, f-diff, f+diff, divs, layers-1)
+
+def freqs(record, divs=90):
+  diff = record.duration/divs
+  for i in range(divs):
+    t = diff*i
+    S = record.sub(t, t+diff)
+    print int(t*100)/100.0, '\t', freq_search(S, 20, 2000)
